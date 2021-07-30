a752185005dcdf1cc83b006c99f248a1c3d73917
aryanmajeed <aryanmajid97@gmail.com>
2021-07-30 20:26:59 +0300

    commit tree 52bc60d5725939addeff0aa46928711673899be2

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default function EyesOnMe() {
+    function blur() {
+        console.log('Hey! Eyes on me!')
+    }
+    function focus() {
+        console.log('Good!')
+    }
+    return (
+        <button onBlur={blur} onFocus={focus}>
+            Eyes on me
+        </button>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import React from 'react'
+
+export default function Keypad({ onChange }) {
+    function onChange() {
+        console.log('Entering password...')
+    }
+    return (
+        <input type="password" placeholder="password" onChange={onChange} />
+    )
+}
